34d4aa43dc9cebf480cf9e4c359545f9cdf906b0
rahulmasquee <rahulmasquee@gmail.com>
2022-01-31 18:15:36 +0530

    trial

--- func.py
+++ func.py
@@ -20,13 +20,28 @@ def read_from_json_file(path):
         logging.getLogger().error("Exception while reading json file" + str(error))
 
 
+class oci_sdk_actions:
+    def __init__(self,region):
+        self.region = region
+        self.signer = oci.auth.signers.get_resource_principals_signer()
+
+    def fetch_ad(self,region_config,aws_region):
+        identity_client = oci.identity.IdentityClient(config={'region': self.region}, signer = self.signer)
+        oci_compartment_id = region_config[aws_region]['oci_compartment_ocid']
+        availability_domains = oci.pagination.list_call_get_all_results(
+            identity_client.list_availability_domains,oci_compartment_id).data
+        logging.getLogger().info("Ad informations" + str(availability_domains))
+
+        
+
+
 def handler(ctx, data: io.BytesIO=None):
     try:
         body = json.loads(data.getvalue())
         logging.getLogger().info("inputs" + str(body))
         logging.getLogger().info("Invoked function with default  image")
-        instance_config = read_from_json_file("/function/instanceconfig.json")
-        region_config =  read_from_json_file("/function/regionconfig.json")
+        instance_config = read_from_json_file("/function/instance_config.json")
+        region_config =  read_from_json_file("/function/region_config.json")
         shape_config = read_from_json_file("/function/shape_config.json")
         image_config = read_from_json_file("/function/image_config.json")
         subnet_config = read_from_json_file("/funcion/subnet_config.json")
@@ -35,6 +50,10 @@ def handler(ctx, data: io.BytesIO=None):
         aws_image_id = body['image-id']
         aws_instance_type = body['instance-type']
         aws_subnet_id = body['subnet-id']
+
+        oci_region = region_config[aws_region]['oci_region']
+        oci_sdk_handler = oci_sdk_actions(oci_region)
+        oci_sdk_handler.fetch_ad(region_config,aws_region)
 
         logging.getLogger().info("ivar"+str(region_config))
         return response.Response(
