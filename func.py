7d9b032fc35a9c663267fefcc18d3f279f95ce1d
rahulmasquee <rahulmasquee@gmail.com>
2022-02-02 19:55:00 +0530

    trial

--- func.py
+++ func.py
@@ -26,7 +26,7 @@ class oci_sdk_actions:
         self.region = region
         self.signer = oci.auth.signers.get_resource_principals_signer()
 
-    def launch_instance(self,oci_instance_shape):
+    def launch_instance(self,oci_instance_shape,oci_subnet_id,oci_image_id):
         try:
     
             logging.getLogger().info("inside launch compute function")
@@ -35,14 +35,14 @@ class oci_sdk_actions:
             launch_instance_response = core_client.launch_instance(
                 launch_instance_details=oci.core.models.LaunchInstanceDetails(
                 availability_domain="Qhab:PHX-AD-1", # This will be dynamically fetch from fetch ad function.
-                compartment_id="ocid1.compartment.oc1..aaaaaaaaievlqkktpe5yumlanr64gnzgi2vokbhrsuz2sddcjooewbqqj5ha", # This is from a map 
+                compartment_id="ocid1.compartment.oc1..aaaaaaaaievlqkktpe5yumlanr64gnzgi2vokbhrsuz2sddcjooewbqqj5ha", # This will be  from a map 
                 shape=oci_instance_shape,
                 create_vnic_details=oci.core.models.CreateVnicDetails(
                     assign_public_ip=True,
-                    subnet_id="ocid1.subnet.oc1.phx.aaaaaaaa5t6wyuvglpzdw3rm6mvtgnjupv22edbxzacra7djem7asv2eufgq"
+                    subnet_id=oci_subnet_id
 
                 ),
-                image_id="ocid1.image.oc1.phx.aaaaaaaaqpk3kgliqneamdnkslngae5x22xf43xqglu6ijrxl32wx3noxtca"
+                image_id=oci_image_id
 
                 ))
             return launch_instance_response
@@ -81,10 +81,12 @@ def handler(ctx, data: io.BytesIO=None):
         logging.getLogger().info('subnet is ' + str(aws_subnet_id))
 
         oci_instance_shape = shape_config[aws_shape_name]
+        oci_subnet_id = subnet_config[aws_subnet_id]
+        oci_image_id = image_config[aws_image_id]
 
         oci_region = 'us-phoenix-1' #Eventually this will come from the compat endpoint handeler 
         oci_sdk_handler = oci_sdk_actions(oci_region)
-        instance_creation_response = oci_sdk_handler.launch_instance(oci_instance_shape)
+        instance_creation_response = oci_sdk_handler.launch_instance(oci_instance_shape,oci_subnet_id,oci_image_id)
         # ad=oci_sdk_handler.fetch_ad(region_config,aws_region)
         # for i in ad:
         #     logging.getLogger().info(str(i['name']))
