339ac047e443c966ab9614758b7fc2b058d4ad9d
rahulmasquee <rahulmasquee@gmail.com>
2022-02-02 18:35:08 +0530

    trial

--- func.py
+++ func.py
@@ -21,10 +21,31 @@ def read_from_json_file(path):
         logging.getLogger().error("Exception while reading json file" + str(error))
 
 
-# class oci_sdk_actions:
-#     def __init__(self,region):
-#         self.region = region
-#         self.signer = oci.auth.signers.get_resource_principals_signer()
+class oci_sdk_actions:
+    def __init__(self,region):
+        self.region = region
+        self.signer = oci.auth.signers.get_resource_principals_signer()
+
+    def launch_instance(self):
+        logging.getLogger().info("inside launch compute function")
+        # Initialize service client with default config file
+        core_client = oci.core.ComputeClient(config={'region': self.region}, signer = self.signer)
+        launch_instance_response = core_client.launch_instance(
+            availability_domain="Qhab:PHX-AD-1",
+            compartment_id="ocid1.compartment.oc1..aaaaaaaaievlqkktpe5yumlanr64gnzgi2vokbhrsuz2sddcjooewbqqj5ha",
+            shape="VM.Standard2.8",
+            create_vnic_details=oci.core.models.CreateVnicDetails(
+                assign_public_ip=True,
+                subnet_id="ocid1.subnet.oc1.phx.aaaaaaaa5t6wyuvglpzdw3rm6mvtgnjupv22edbxzacra7djem7asv2eufgq"
+
+            ),
+            image_id="ocid1.image.oc1.phx.aaaaaaaaqpk3kgliqneamdnkslngae5x22xf43xqglu6ijrxl32wx3noxtca"
+
+            )
+            logging.getLogger().info(str(launch_instance_response.data))
+
+
+
 
 #     def fetch_ad(self,region_config,aws_region):
 #         logging.getLogger().info("Inside fetch Ad info function")
@@ -53,8 +74,9 @@ def handler(ctx, data: io.BytesIO=None):
         aws_subnet_id = body.split('SubnetId=')[1].split('&')[0]
         logging.getLogger().info('subnet is ' + str(aws_subnet_id))
 
-        # oci_region = region_config[aws_region]['oci_region']
-        # oci_sdk_handler = oci_sdk_actions(oci_region)
+        oci_region = 'us-phoenix-1' #Eventually this will come from the compat endpoint handeler 
+        oci_sdk_handler = oci_sdk_actions(oci_region)
+        oci_sdk_handler.launch_instance()
         # ad=oci_sdk_handler.fetch_ad(region_config,aws_region)
         # for i in ad:
         #     logging.getLogger().info(str(i['name']))
